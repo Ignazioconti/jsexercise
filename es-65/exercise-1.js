345955907d6f1146d6cd083e402f3e0c6fb39d3b
Ignazioconti <108429510+Ignazioconti@users.noreply.github.com>
2023-01-11 14:08:58 +0100

    Rest parameters and spread syntax

--- es-65/exercise-1.js
+++ es-65/exercise-1.js
@@ -1,6 +1,6 @@
-function sum(num1, num2, num3, num4, num5) {
-  return num1 + num2 + num3, num4, num5;
+function sum(...numbers) {
+  return numbers.reduce((preValue, currentValue) => {
+    return preValue + currentValue;
+  });
 }
-
-const numbers = [1, 2, 3, 4, 5];
-console.log(sum(...numbers));
+console.log(sum(1, 2, 3, 4, 5));
