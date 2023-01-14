1c26f25fd36f48a1e4843925dccd542553349c22
Ignazioconti <108429510+Ignazioconti@users.noreply.github.com>
2023-01-14 18:46:31 +0100

    Getters and Setters

---
+++ es-58/exercise-7.js
@@ -0,0 +1,35 @@
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  get firstname() {
+    return this.firstName;
+  }
+  set firstname(name) {
+    this.firstName = name;
+  }
+  get lastname() {
+    return this.lastName;
+  }
+  set lastname(name) {
+    this.lastName = name;
+  }
+  get ages() {
+    return this.age;
+  }
+  set ages(ages) {
+    this.age = ages;
+  }
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const person = new Person("Mario", "Rossi", 25);
+console.log(person.fullName);
+
+person.firstName = "Maria";
+person.lastName = "Verdi";
+console.log(person.fullName);
